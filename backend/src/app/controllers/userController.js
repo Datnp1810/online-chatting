65e1a3837bbdb90bfd610fdcc5bf14a24fade8c5
Datnp1810 <147915990+Datnp1810@users.noreply.github.com>
2025-01-05 19:57:38 +0700

    test

--- backend/src/app/controllers/userController.js
+++ backend/src/app/controllers/userController.js
@@ -11,6 +11,7 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(200).json(filteredUsers);
   } catch (error) {
     console.error("Error in getUsersForSidebar: ", error.message);
+    console.error("Test conflic");
     res.status(500).json({ error: "Internal server error" });
   }
 };
